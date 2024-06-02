cb06dc6169cb9de8cf5f6581993746df17d09d70
fishyer <yutianran666@gmail.com>
2024-06-02 17:12:12 +0800

    init

---
+++ apscheduler_client.py
@@ -0,0 +1,24 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+from ezlogger import print, logger
+from main import dida2wf, initwf
+
+
+def main():
+    print(__file__)
+    initwf()
+    # 先执行一次
+    dida2wf()
+    # 创建后台调度器,间隔10秒钟执行一次
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(dida2wf, "cron", second="*/10")
+    scheduler.start()
+    try:
+        while True:
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+
+
+if __name__ == "__main__":
+    main()
